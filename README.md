aa802b294acab2dde74c09ea8996b2c2f0d69b3d
jdambra10 <98533715+jdambra10@users.noreply.github.com>
2022-02-08 15:00:02 +0000

    finished activity

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 
 ## Team Members
 
-* Team Member 1: John Dambra
+* Team Member 1: Erin Zhao
 * Team Member 2: Rachel Todd
-* Team Member 3: Erin Zhao
+* Team Member 3: John Dambra
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+mylist = []
+
+mylist.append(int(input("Input a number: "))) 
+mylist.append(int(input("Input a number: "))) 
+mylist.append(int(input("Input a number: "))) 
+mylist.append(int(input("Input a number: "))) 
+
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+
+mylist[0], mylist[3] = mylist[3], mylist[0]
